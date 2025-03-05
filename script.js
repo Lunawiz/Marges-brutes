399437fcd133ac59e9627d7a82fc61ec09616dc8
Lunawiz <alex05michel12@gmail.com>
2025-03-05 17:32:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(".button").click(function () {
-    //a random number is generated
-    //that number chooses which string to display
-    //the string displays in html
-    let numVal = Math.floor(Math.random() * 10)
+
+    let numVal = Math.floor(Math.random() * 41)
     const expr = numVal
     switch (expr) {
       case 1:
@@ -45,14 +43,134 @@ $(".button").click(function () {
         console.log("Espaces en tension")
         $("#show-me").html("Espaces en tension")
         break
+     
+        case 11:
+          console.log("Mise en page oracle")
+          $("#show-me").html("Mise en page oracle")
+          break
+        case 12:
+          console.log("Anatomie d’une page")
+          $("#show-me").html("Anatomie d’une pagee")
+          break
+        case 13:
+          console.log("Chorégraphie graphique")
+          $("#show-me").html("Chorégraphie graphique")
+          break
+        case 14:
+          console.log("Archipel typographique")
+          $("#show-me").html("Archipel typographique")
+          break
+        case 15:
+          console.log("Alphabet migrant")
+          $("#show-me").html("Alphabet migrant")
+          break
+        case 16:
+          console.log("Échos textuels")
+          $("#show-me").html("Échos textuels")
+          break
+          case 17:
+          console.log("Bruit parasite")
+          $("#show-me").html("Bruit parasite")
+          break
+        case 18:
+          console.log("Typographie échouée")
+          $("#show-me").html("Typographie échouée")
+          break
+        case 19:
+          console.log("Fantômes typographiques")
+          $("#show-me").html("Contraintes centrifuges")
+          break
+        case 20:
+          console.log("Cartographie du texte")
+          $("#show-me").html("Cartographie du texte")
+          break
+
+
+          case 21:
+            console.log("Pliage aléatoire")
+            $("#show-me").html("Pliage aléatoire")
+            break
+          case 22:
+            console.log("Typo-écoute")
+            $("#show-me").html("Typo-écoute")
+            break
+          case 23:
+            console.log("Ombres portées")
+            $("#show-me").html("Ombres portées")
+            break
+          case 24:
+            console.log("Érosion typographique")
+            $("#show-me").html("Érosion typographique")
+            break
+          case 25:
+            console.log("Fragmentation syntaxique")
+            $("#show-me").html("Fragmentation syntaxique")
+            break
+          case 26:
+            console.log("Écriture suspendue")
+            $("#show-me").html("Écriture suspendue")
+            break
+            case 27:
+            console.log("Parasitages visuels")
+            $("#show-me").html("Parasitages visuels")
+            break
+          case 28:
+            console.log("Résonances graphiques")
+            $("#show-me").html("Résonances graphiques")
+            break
+          case 29:
+            console.log("Disparitions éditoriales")
+            $("#show-me").html("Disparitions éditoriales")
+            break
+          case 30:
+            console.log("Ratures constructives")
+            $("#show-me").html("Ratures constructives")
+            break
+
+            case 31:
+              console.log("Transferts et empreintes")
+              $("#show-me").html("Transferts et empreintes")
+              break
+            case 32:
+              console.log("Mise en page télépathique")
+              $("#show-me").html("Mise en page télépathique")
+              break
+            case 33:
+              console.log("Strates narratives")
+              $("#show-me").html("Strates narratives")
+              break
+            case 34:
+              console.log("Collisions typographiques")
+              $("#show-me").html("Collisions typographiques")
+              break
+            case 35:
+              console.log("Mise en page synesthésique")
+              $("#show-me").html("Mise en page synesthésiques")
+              break
+            case 36:
+              console.log("Contrôle déséquilibré")
+              $("#show-me").html("Contrôle déséquilibré")
+              break
+              case 37:
+              console.log("Rythmes éditoriaux")
+              $("#show-me").html("Rythmes éditoriaux")
+              break
+            case 38:
+              console.log("Fragments mouvants")
+              $("#show-me").html("Fragments mouvants")
+              break
+            case 39:
+              console.log("Ruines éditoriales")
+              $("#show-me").html("Ruines éditoriales")
+              break
+            case 40:
+              console.log("Mise en page spectrale")
+              $("#show-me").html("Mise en page spectrale")
+              break
+
       default:
         console.log("Lecture oblique")
         $("#show-me").html("Lecture oblique")
     }
   })
   
-  // Math.floor(Math.random() * 10) + 1
-  
-  // if(something=true){
-  //   then do some stuff
-  // }
